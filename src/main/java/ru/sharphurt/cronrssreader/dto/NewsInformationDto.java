10f425e2c2bfdb13a7ac9aff690ad9ef38c8f6e4
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2024-03-02 01:27:20 +0500

    Added deserializer

--- src/main/java/ru/sharphurt/cronrssreader/dto/NewsInformationDto.java
+++ src/main/java/ru/sharphurt/cronrssreader/dto/NewsInformationDto.java
@@ -1,9 +1,11 @@
 package ru.sharphurt.cronrssreader.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.sharphurt.cronrssreader.serialization.ZonedDateTimeDeserializer;
 import ru.sharphurt.cronrssreader.serialization.ZonedDateTimeSerializer;
 
 import java.time.ZonedDateTime;
@@ -18,5 +20,6 @@ public class NewsInformationDto {
     private String description;
 
     @JsonSerialize(using = ZonedDateTimeSerializer.class)
+    @JsonDeserialize(using = ZonedDateTimeDeserializer.class)
     private ZonedDateTime publicationDate;
 }
